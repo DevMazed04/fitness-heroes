4799f55a0157b62be3037a843ebd93ceba0ea757
DevMazed04 <mdmazedislam2684@gmail.com>
2022-09-29 12:35:24 +0600

    userInfo component added

--- src/components/Gym/Gym.js
+++ src/components/Gym/Gym.js
@@ -1,49 +1,34 @@
-import React, { useEffect, useState } from 'react';
-import Exercise from '../Exercise/Exercise';
-import myImg from '../../images/devMazed.png'
-import './Gym.css'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faChalkboardUser } from '@fortawesome/free-solid-svg-icons'
+import React, { useEffect, useState } from "react";
+import Exercise from "../Exercise/Exercise";
+import UserInfo from "../UserInfo/UserInfo";
+import "./Gym.css";
+
 
 const Gym = () => {
-   const [exercises, setExercises] = useState([]);
+  const [exercises, setExercises] = useState([]);
 
-   useEffect(() => {
-      fetch('data.json')
-         .then(res => res.json())
-         .then(data => setExercises(data))
-   }, [])
+  useEffect(() => {
+    fetch("data.json")
+      .then((res) => res.json())
+      .then((data) => setExercises(data));
+  }, []);
 
-   return (
-      <div className='gym-container'>
-         <div className='exercises-container'>
-            <h4 className='mb-4'>Best exercises for health and fitness</h4>
-            <div className="row row-cols-1 row-cols-md-2 row-cols-lg-3 g-4 mb-5 card-container">
-               {
-                  exercises.map(exercise => <Exercise
-                     key={exercise.id}
-                     exercise={exercise}>
-                  </Exercise>)
-               }
-            </div>
-         </div>
-         <div className='exercise-details-container bg-white p-4'>
-            <div className='d-flex align-items-center gap-3'>
-               <div>
-                  <img className='rounded-pill mb-2' src={myImg} alt="devmazed-img" width="50px" />
-               </div>
-               <div className=''>
-                  <h4 className='mb-1'>Md. Mazedul Islam</h4>
-                  <div className='d-flex gap-2'>
-                     <p className='opacity-75'> <FontAwesomeIcon icon={faChalkboardUser}></FontAwesomeIcon></p>
-                     <small className='text-secondary'>Web Developer</small>
-                  </div>
-               </div>
-               
-            </div>
-         </div>
+  return (
+    <section className="gym-container">
+      <div className="exercises-container">
+        <h4 className="mb-4">Best exercises for health and fitness</h4>
+        <div className="row row-cols-1 row-cols-md-2 row-cols-lg-3 g-4 mb-5 card-container">
+          {exercises.map((exercise) => (
+            <Exercise key={exercise.id} exercise={exercise}></Exercise>
+          ))}
+        </div>
       </div>
-   );
+
+      <div className="exercise-details-container bg-white p-4 rounded-3">
+        <UserInfo></UserInfo>
+      </div>
+    </section>
+  );
 };
 
 export default Gym;
---
+++ src/components/UserInfo/UserInfo.js
@@ -0,0 +1,56 @@
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faChalkboardUser } from "@fortawesome/free-solid-svg-icons";
+import myImg from "../../images/devMazed.png";
+
+const UserInfo = () => {
+  return (
+    <div>
+      <div className="d-flex align-items-center gap-3">
+        <div>
+          <img
+            className="rounded-pill mb-2"
+            src={myImg}
+            alt="devmazed-img"
+            width="60px"
+          />
+        </div>
+        <div>
+          <h4 className="mb-1">Md. Mazedul Islam</h4>
+          <div className="d-flex gap-2">
+            <p className="opacity-75">
+              <FontAwesomeIcon icon={faChalkboardUser}></FontAwesomeIcon>
+            </p>
+            <small className="text-secondary">Web Developer</small>
+          </div>
+        </div>
+      </div>
+
+      <div className="bg-light rounded-3 mt-3">
+        <div className="d-flex justify-content-evenly gap-4">
+          <p>
+            <h4 className="mb-0 mt-3 fw-bold">75
+              <sub className="text-secondary fw-normal"> <small>Kg</small>
+              </sub>
+            </h4>Weight
+          </p>
+          <p>
+            <h4 className="mb-0 mt-3 fw-bold">5.8
+              <sup className="text-secondary fw-normal">
+                <small>"</small>
+              </sup>
+            </h4>Height
+          </p>
+          <p>
+            <h4 className="mb-0 mt-3 fw-bold">26
+              <sub className="text-secondary fw-normal">
+                <small>Yrs</small>
+              </sub></h4>Age
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default UserInfo;
